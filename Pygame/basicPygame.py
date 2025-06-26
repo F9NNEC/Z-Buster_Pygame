783bcbd1d220c4ad1436547c8416292bac0223d0
Aditya Tri Andriyansyah <adityatriandriyansyah9@gmail.com>
2025-06-26 13:48:38 +0700

    Update

--- Pygame/basicPygame.py
+++ Pygame/basicPygame.py
@@ -11,6 +11,9 @@ pygame.display.set_caption("Fennec")
 icon = pygame.image.load('fnnc.png')
 pygame.display.set_icon(icon)
 
+# background
+background = pygame.image.load('spaceBackground.png')
+
 
 # player
 adit = pygame.image.load('char.png')
@@ -24,8 +27,9 @@ def player(x, y):
 # enemy
 alien = pygame.image.load('alien.png')
 enemyX = random.randint(10,790)
-enemyY = 100
-enemyX_change = 0
+enemyY = 50
+enemyX_change = 2
+enemyY_change = 40
 
 def enemy(x, y):
     screen.blit(alien, (x, y))
@@ -36,22 +40,32 @@ while running:
     # window color red, green, blue
     screen.fill((0, 0, 0))
 
-    # exit button
+    # background
+    screen.blit(background, (0, 0))
+
     for event in pygame.event.get():
+    # exit button
         if event.type == pygame.QUIT:
             running = False
+    
+    # check if mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('Pressed')
+        if event.type == pygame.MOUSEBUTTONUP:
+            print('Released')
 
     # if keystroke is pressed check wheter its right or left
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -0.5
+            if event.key == pygame.K_a:
+                playerX_change = -4
 
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 0.5
+            if event.key == pygame.K_d:
+                playerX_change = 4
+
 
     # if keystroke has been released
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
                 playerX_change = 0
         
     # make the player move
@@ -59,9 +73,20 @@ while running:
 
     # check if character touch edges
     if playerX <= 1:
-        playerX_change = 0
+        playerX = 1
     elif playerX >= 736:
-        playerX_change = 0
+        playerX = 736
+
+    # make the enemy move
+    enemyX += enemyX_change
+
+    # check if enemy touch edges
+    if enemyX <= 1:
+        enemyX_change = 2
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -2
+        enemyY += enemyY_change
 
     # make player appear to window
     player(playerX, playerY)
